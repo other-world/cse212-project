b0874b1cdb1cd9434cfe6ab05363732e942ec9aa
other-world <jamie@other-world.org>
2026-03-11 19:50:25 -0600

    Debugged the SimpleQueue.cs for week 02

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -1,4 +1,5 @@
-﻿public class SimpleQueue {
+﻿using System.Diagnostics;
+public class SimpleQueue {
     public static void Run() {
         // Test Cases
 
@@ -66,8 +67,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
